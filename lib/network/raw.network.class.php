fb56d6f359eab8dcc5d38b3e57556acff790f92b
Martijn Bogaard <martijn@bogaard.eu>
2009-06-20 20:31:31 +0000

    git-svn-id: http://prnl.googlecode.com/svn/trunk@8 7e3b58f6-5d23-11de-b3c1-0fd540ce45e4

---
+++ lib/network/raw.network.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Raw Network Class
+ * 
+ * PHP Raw Network Library
+ * (c) 2009 Kenneth van Hooff & Martijn Bogaard
+ */
+
+class RawNetworkClass {
+	private $_socket;
+	
+	public function createRawSocket(int $ipProtocol, int $contentProtocol) {
+		if ($ipProtocol == PROT_IPv4)
+			$socketFamiliy = AF_INET;
+		else if ($ipProtocol == PROT_IPv4)
+			$socketFamiliy = AF_INET6;
+		else
+			new Exception("Unknown IP protocol");
+			
+		$this->_socket = socket_create($socketFamiliy, SOCK_RAW, $contentProtocol);
+		
+		if (!$this->_socket) {
+			throw new Exception(socket_strerror(socket_last_error()));
+		}
+	}
+	
+	public function closeSocket() {
+		if (is_resource($this->_socket)) {
+			socket_close($this->_socket);
+			
+			$this->_socket = null;
+		}
+	}
+}
+
+?>
