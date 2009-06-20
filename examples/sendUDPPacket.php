b20a66b6f86bea9bbee89e430273ee44337a0f01
Martijn Bogaard <martijn@bogaard.eu>
2009-06-20 20:12:18 +0000

    git-svn-id: http://prnl.googlecode.com/svn/trunk@4 7e3b58f6-5d23-11de-b3c1-0fd540ce45e4

--- examples/sendUDPPacket.php
+++ examples/sendUDPPacket.php
@@ -9,6 +9,6 @@ chdir(dirname(__FILE__)); //change working dir to the script dir
 require_once('../lib/lib.prnl.php');
 
 if ($_SERVER["argc"] != 2)
-	die('./script <ip> <port>');
+	die('./script <ip> <port>'.PHP_EOL);
 	
 var_dump($_SERVER['argv']);
