0876a1ddd3dddf81d0eb4dd783b60bc420bcb771
Martijn Bogaard <martijn@bogaard.eu>
2009-06-20 21:03:56 +0000

    git-svn-id: http://prnl.googlecode.com/svn/trunk@26 7e3b58f6-5d23-11de-b3c1-0fd540ce45e4

---
+++ examples/dumpRawPackets.php
@@ -0,0 +1,17 @@
+<?php
+
+if (php_sapi_name() != 'cli') {
+	die('This script can only run from the commandline!'.PHP_EOL);
+}
+
+chdir(dirname(__FILE__)); //change working dir to the script dir
+
+require_once('../lib/lib.prnl.php');
+
+
+$rawNetworkManager = new RawNetwork();
+$rawNetworkManager->createRawSocket(PROT_IPv4, PROT_UDP);
+
+while ($packet = $rawNetworkManager->readPacket()) {
+	$packet->dumpPacket();
+}
