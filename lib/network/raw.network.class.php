27075b64342fe664197bd217ac4d62772985add7
Martijn Bogaard <martijn@bogaard.eu>
2009-06-20 20:32:17 +0000

    git-svn-id: http://prnl.googlecode.com/svn/trunk@9 7e3b58f6-5d23-11de-b3c1-0fd540ce45e4

--- lib/network/raw.network.class.php
+++ lib/network/raw.network.class.php
@@ -25,6 +25,12 @@ class RawNetworkClass {
 		}
 	}
 	
+	public function setSendCustomIPHeader() {
+		if (!$this->_socket) {
+			throw new Exception('Socket not yet opened!');
+		}
+	}
+	
 	public function closeSocket() {
 		if (is_resource($this->_socket)) {
 			socket_close($this->_socket);
