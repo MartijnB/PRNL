6a1a3b9edb67d76a27f6cfab10985a4530cd4fb8
Martijn Bogaard <martijn@bogaard.eu>
2009-06-20 20:11:42 +0000

    git-svn-id: http://prnl.googlecode.com/svn/trunk@3 7e3b58f6-5d23-11de-b3c1-0fd540ce45e4

---
+++ examples/sendUDPPacket.php
@@ -0,0 +1,14 @@
+<?php
+
+if (php_sapi_name() != 'cli') {
+	die('This script can only run from the commandline!'.PHP_EOL);
+}
+
+chdir(dirname(__FILE__)); //change working dir to the script dir
+
+require_once('../lib/lib.prnl.php');
+
+if ($_SERVER["argc"] != 2)
+	die('./script <ip> <port>');
+	
+var_dump($_SERVER['argv']);
