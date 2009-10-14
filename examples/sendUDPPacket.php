c1ab8a26c179d7cfd5b93ea80f32df31ef963406
Martijn Bogaard <martijn@bogaard.eu>
2009-10-14 18:36:43 +0000

    Updated default udp send script
    git-svn-id: http://prnl.googlecode.com/svn/trunk@162 7e3b58f6-5d23-11de-b3c1-0fd540ce45e4

--- examples/sendUDPPacket.php
+++ examples/sendUDPPacket.php
@@ -14,20 +14,16 @@ $rawNetworkManager = new RawIPNetwork();						// Create new IPNetwork
 $rawNetworkManager->createIPSocket(PROT_IPv4, PROT_UDP);		// Create new IP socket (IPv4 - UDP)
 
 $rawIPv4Package = new IPv4ProtocolPacket();						// Create new IPv4 Packet
-$rawIPv4Package->setLength(0);									// Set the length 0, we will calculate it later
-$rawIPv4Package->setIdSequence(1);								// Set the Sequence
+$rawIPv4Package->setIdSequence(0);								// Set the Sequence
 $rawIPv4Package->setOffset(0);									// Set the Offset
 $rawIPv4Package->setTTL(255);									// Set the Time To Live
 $rawIPv4Package->setProtocol(PROT_UDP);							// Set protocol to UDP (17)
-$rawIPv4Package->setChecksum(0);								// Set the checksum 0, we will calcualte it later
 $rawIPv4Package->setSrcIP("127.0.0.1");
 $rawIPv4Package->setDstIP($ip);
 
 $rawUDPPackage = new UDPProtocolPacket();						// Create new UDP Packet
 $rawUDPPackage->setSrcPort($port);								// Set Source port
 $rawUDPPackage->setDstPort($port);								// Set Destination port
-$rawUDPPackage->setLength(0);									// Set the length 0, we will calculate it later
-$rawUDPPackage->setChecksum(0);									// Set the checksum 0, we will calcualte it later
 $rawUDPPackage->setData("Hello World");							// Set the data
 
 print "IP package: ";
